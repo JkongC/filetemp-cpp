96be9c4751f47bef6b314a6cb20121edffe9a052
JkongC <2637605680@qq.com>
2026-02-03 14:54:38 +0800

    Remove c/cxxstd limitation

--- src/main.cpp
+++ src/main.cpp
@@ -12,16 +12,10 @@ int main(int argc, char **argv)
     ArgumentParser cmake_parser{ "cmake", "", default_arguments::help };
     cmake_parser.add_argument("directory").help("The output directory").default_value<std::string>(".");
     cmake_parser.add_argument("--version", "-v").help("Minimum cmake version").default_value<std::string>("3.0");
-    cmake_parser.add_argument("--cstd", "-c").help("C standard").default_value<int>(99).choices("99", "11", "17", "23");
-    cmake_parser.add_argument("--cxxstd", "-C")
-        .help("C++ standard")
-        .default_value<int>(20)
-        .choices("99", "11", "14", "17", "20", "23", "26");
+    cmake_parser.add_argument("--cstd", "-c").help("C standard").scan<'i', int>().default_value<int>(99);
+    cmake_parser.add_argument("--cxxstd", "-C").help("C++ standard").scan<'i', int>().default_value<int>(20);
     cmake_parser.add_argument("--project", "-p").help("Project and executable name").default_value<std::string>("foo");
-    cmake_parser.add_argument("--main-lang", "-m")
-        .help("Main language of the project")
-        .default_value("CXX")
-        .choices("CXX", "C");
+    cmake_parser.add_argument("--main-lang", "-m").help("Main language of the project").default_value("CXX");
     cmake_parser.add_argument("--export-commands", "-e").help("Export compile commands").flag();
     cmake_parser.add_argument("--generate-src", "-g").help("Generate source file").flag();
     cmake_parser.add_argument("--show", "-s").help("Show output to console").flag();
